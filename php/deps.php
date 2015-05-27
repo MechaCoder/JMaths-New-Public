ab509dbb8650ceae004f8939f25dfa00c0a4b264
MechaCoder <talktome1224@gmail.com>
2015-05-27 16:13:49 +0100

    file system done

---
+++ php/deps.php
@@ -0,0 +1,5 @@
+<!-- CDN include -->
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<script src='http://code.jquery.com/jquery-git2.min.js'></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
