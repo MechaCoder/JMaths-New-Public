dae24a22c1513a78aceabee09863a2b9d1ef61a8
MechaCoder <talktome1224@gmail.com>
2015-05-27 15:44:06 +0100

    setting basicframe works

---
+++ head.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<?php include('deps.php') ?>
+		<title> <?php echo($page_title); ?> </title>
+	</head>
+	<body>
+		<header class='container'>
+			<div> <h1 class='cust-page-title'> <?php echo($page_tile;) ?></div>
+			<nav class='cust-page-nav'>
+				<a href=''><div>App</div></a>
+				<a href=''><div>Information</div></a>
+			</nav>
+		</header>
---
+++ pageTemplate.php
@@ -0,0 +1,7 @@
+//page setup
+<?php 
+$page_title = 'page title';
+?> 
+<?php include('head.php');// this file dose not include a close body tag ?>
+
+<?php include('foot.php'); ?>
