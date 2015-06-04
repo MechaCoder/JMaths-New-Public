ceb4dc4643362bb549a36ed21971963bb4cca3dc
MechaCoder <talktome1224@gmail.com>
2015-06-04 12:29:03 +0100

    playing around js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 console.log("m.p. > start");
 
+var mat_stuff = ()
+
 var CanvasIDs = ['number1units', 'number1tens', 'number1ten' ,'number2units']
 
-function render(ren, intv){
+function renderTest(ren, intv){
 	var can1u = document.getElementById(ren);
 	can1u = can1u.getContext('2d');
 	can1u.beginPath();
@@ -14,6 +16,8 @@ function render(ren, intv){
 	console.log(intv);
 }
 
+
+
 function ClearCanvas(CanvasID){
 	var CanID = document.getElementById(CanvasID);
 	var CanC  = CanID.getContext('2d');
@@ -21,13 +25,17 @@ function ClearCanvas(CanvasID){
 }
 
 function clearAllCanvas(){
-	for (i = 0, i < CanvasIDs.length(), i++1){
-		ClearCanvas(CanvasIDs[i]);
-	}
+	var canAmount = CanvasIDs.length;
+	console.log('var - canAmount = ' + canAmount);
+
 }
 
 $(document).ready(function() {
+	clearAllCanvas();
+	render(CanvasIDs[0], 1);
+	
 
+	console.log("m.p. > finshed");
 })
 
-console.log("m.p. > finished");
+console.log("m.p. > loaded");
