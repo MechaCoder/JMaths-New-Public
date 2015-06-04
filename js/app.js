ee486026b1181b9a8d6bf02d3f1471b81a79b69a
MechaCoder <talktome1224@gmail.com>
2015-06-04 22:11:04 +0100

    stabelising

--- js/app.js
+++ js/app.js
@@ -1,38 +1,29 @@
-console.log("m.p. > start");
+//console.log("m.p. > start");
 var CanvasIDs = ['number1units', 'number1tens', 'number1ten' ,'number2units']
+
 var canvasData = { //one place to customeion useig
-	Context: '2d', 
-	lineWidth: 4, 
+	Context: "2d", 
+	lineWidth: 8, 
 	lineColors: ['red','black','blue','pink'],
+	canvasSize: []
+};
+//write a fuction that captures the canvase size
+function canvasDataGet(){
+	var x = document.getElementById( CanvasIDs[0] );
+	canvasData['canvasSize'][0] = x.width;
+	canvasData['canvasSize'][1] = x.height;
 }
-
-
 function renderTest(ren, intv){
 	var can1u = document.getElementById(ren);
-	can1u = can1u.getContext('2d');
+	can1u = can1u.getContext( canvasData['Context'] );
 	can1u.beginPath();
 	can1u.moveTo(0,0);
-	can1u.lineTo(1000,1000);
-	can1u.lineWidth = 4;
-	can1u.strokeStyle = '#FB8136';
+	can1u.lineTo( parseInt(canvasData['canvasSize'][0]), parseInt(canvasData['canvasSize'][1]) ) ;
+	can1u.lineWidth = canvasData['lineWidth'];
+	can1u.strokeStyle = canvasData['lineColors'][0];
 	can1u.stroke();
-	console.log("intv - " + intv);
+	//console.log("intv - " + intv);
 }
-
-function FullRender(int1, int2, int3, int4){
-	var canvasObject1 = CanvasIDs[0];
-	
-	var startCords = [0,0];
-	var endCords   = [1000,1000];
-
-	canvasObject1 = getContext( canvasData[Context] );
-	canvasObject1.beginPath();
-	var loopNum = 0;
-	while(int1 != loopNum){//makes the lines for each canvas
-
-	}
-}
-
 function ClearCanvas(CanvasID){
 	var CanID = document.getElementById(CanvasID);
 	var CanC  = CanID.getContext('2d');
@@ -41,14 +32,24 @@ function ClearCanvas(CanvasID){
 
 function clearAllCanvas(){
 	var canAmount = CanvasIDs.length;
-	console.log('var - canAmount = ' + canAmount);
+	//console.log('var - canAmount = ' + canAmount);
 
 }
 
+function FullRender(int1, int2, int3, int4){
+	var canvasObject1 = CanvasIDs[0];
+	canvasObject1 = getContext( canvasData[Context] );
+	canvasObject1.beginPath();
+}	
+
+
 $(document).ready(function() {
 	clearAllCanvas();
+	canvasDataGet();
 	renderTest(CanvasIDs[0], 1);
-	console.log("m.p. > finshed");
-})
+	
+	//console.log( canvasData['canvasSize'] );
 
-console.log("m.p. > loaded");
+	//console.log("m.p. > finshed");
+})
+//console.log("m.p. > loaded");
