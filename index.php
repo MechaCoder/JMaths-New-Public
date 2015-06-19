f8167c5bb4a7ad2962098f94b515ea13bc29a63a
MechaCoder <talktome1224@gmail.com>
2015-06-19 15:01:40 +0100

    made a index to server to work

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+$page_title = 'Jmaths | App';
+$appJS = False;
+?> 
+<?php include('head.php');// this file dose not include a close body tag ?>
+
+<div>
+thise is the index files
+
+<a href='./app'>click</a>
+</div>
+<?php include('foot.php'); ?>
